3a9ce32ad0f5535f7731beabe198f8bfe386a7c4
Keegan <keegankevern.work@gmail.com>
2025-05-17 21:48:32 +0100

    Installed major dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxtjs/tailwindcss'
-  ]
-})
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@nuxt/test-utils/module",
+  ],
+});
