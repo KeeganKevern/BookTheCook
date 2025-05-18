a3d758fe46d71c8d40a9c93932998f0f4b8c09a8
Keegan <keegankevern.work@gmail.com>
2025-05-18 12:19:37 +0100

    Initialised router and created routes

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <nav>
+      <RouterLink to="/">Go to Home</RouterLink>
+      <RouterLink to="/about">Go to About</RouterLink>
+    </nav>
+
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
