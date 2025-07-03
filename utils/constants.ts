e620cf72447bda6d8c81fa24e1b82240f1aece34
Keegan <keegankevern.work@gmail.com>
2025-07-03 20:58:05 +0100

    added constants.ts, and minor changes, to close first PR

---
+++ utils/constants.ts
@@ -0,0 +1,6 @@
+export const navFooterData = [
+  { navRoute: "/MeetTheChef", iconName: "icon-park-solid:chef-hat-one" },
+  { navRoute: "/", iconName: "dashicons:admin-home" },
+  { navRoute: "/Booking", iconName: "mdi:calendar-month-outline" },
+  { navRoute: "/UserProfile", iconName: "mdi:account" },
+];
