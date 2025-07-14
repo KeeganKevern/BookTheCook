470c8d4f1c97ec3c640821177541762e440bcbd9
Keegan <keegankevern.work@gmail.com>
2025-07-14 19:55:50 +0100

    added a basic signUp connection for Firebase. tn-BTC-13

--- stores/AuthStore.js
+++ stores/AuthStore.js
@@ -1,30 +1,38 @@
 import { defineStore } from "pinia";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+//Get the Firebase Auth instance
+const auth = getAuth();
 
 export const useAuthStore = defineStore("auth", () => {
   const user = ref(null);
-  const error = ref(null);
+  const error = (ref < string) | (null > null);
 
-  const login = async (email, password) => {
+  const handleAuthentication = async (authFn, email, password) => {
     try {
-      const auth = getAuth();
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      user.value = userCredential.user.uid;
+      const userCredential = await authFn(auth, email, password);
+      user.value = userCredential.user;
       error.value = null;
-      console.log("User logged in:", user.value);
+      console.log("User logged in:", user.value.uid);
     } catch (err) {
-      console.error("Login error:", err.code, err.message);
-      error.value = err.message;
+      console.error("Auth error:", err.code, err.message);
+      error.value = err.message || "An unknown error occurred";
     }
   };
+  const login = (email, password) =>
+    handleAuthentication(signInWithEmailAndPassword, email, password);
+
+  const signUp = (email, password) =>
+    handleAuthentication(createUserWithEmailAndPassword, email, password);
 
   return {
     user,
     error,
     login,
+    signUp,
   };
 });
